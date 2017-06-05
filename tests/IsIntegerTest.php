dca71330ac565afb8544e5524133ed5236b9204b
Paul Daniel Iway <paul.iway@bridgetechnologypartners.com>
2017-06-05 09:43:19 +0800

    added test

--- tests/IsIntegerTest.php
+++ tests/IsIntegerTest.php
@@ -2,6 +2,7 @@
 namespace Logitick\Contract\Tests;
 
 use \Logitick\Contract\Contract;
+use \Logitick\Contract\Type\IsInteger;
 
 class IsIntegerTest extends \PHPUnit_Framework_TestCase {
 
@@ -9,4 +10,20 @@ class IsIntegerTest extends \PHPUnit_Framework_TestCase {
         $contract = Contract::isInt(12);
         $this->assertInstanceOf('\\Logitick\\Contract\\Type\\IsInteger', $contract);
     }
+
+    public function testIsIntegerCheck() {
+        $contract = new IsInteger(12);
+	$this->assertTrue($contract->check());
+    }
+
+    public function testIsIntegerCheckOnString() {
+        $contract = new IsInteger('123');
+	$this->assertfalse($contract->check());
+    }
+
+    public function testIsIntegerCheckOnDouble() {
+        $contract = new IsInteger(1.2);
+	$this->assertFalse($contract->meetsContract());
+    }
+
 }
